f6a0fb9241a7e6380be7698b9e6e6524cc14c1e3
Oleksandr Prus <cheepaxxa2003@gmail.com>
2026-03-31 23:20:38 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -15,29 +15,29 @@ class Distance:
         result = self.km + self._get_km(other)
         return Distance(result)
 
-    def __iadd__(self, other: "Distance") -> float:
+    def __iadd__(self, other: "Distance" | int | float) -> "Distance":
         self.km += self._get_km(other)
         return self
 
-    def __mul__(self, multiplier: int) -> "Distance":
+    def __mul__(self, multiplier: int | float) -> "Distance":
         result = self.km * multiplier
         return Distance(result)
 
-    def __truediv__(self, divider: int) -> "Distance":
+    def __truediv__(self, divider: int | float) -> "Distance":
         result = round(self.km / divider, 2)
         return Distance(result)
 
-    def __lt__(self, other: "Distance") -> bool:
+    def __lt__(self, other: "Distance" | int | float) -> bool:
         return self.km < self._get_km(other)
 
-    def __gt__(self, other: "Distance") -> bool:
+    def __gt__(self, other: "Distance" | int | float) -> bool:
         return self.km > self._get_km(other)
 
-    def __eq__(self, other: "Distance") -> bool:
+    def __eq__(self, other: "Distance" | int | float) -> bool:
         return self.km == self._get_km(other)
 
-    def __le__(self, other: "Distance") -> bool:
+    def __le__(self, other: "Distance" | int | float) -> bool:
         return self.km <= self._get_km(other)
 
-    def __ge__(self, other: "Distance") -> bool:
+    def __ge__(self, other: "Distance" | int | float) -> bool:
         return self.km >= self._get_km(other)
